58e0193de82b742915656fc834d3a99a1b00fb52
Amos-ongeri <amosongeri041@gmail.com>
2025-12-16 17:37:59 +0300

    initial commit

---
+++ lib/pages/loading.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:world_time/services/world_time.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+
+class Loading extends StatefulWidget {
+  const Loading({super.key});
+
+  @override
+  State<Loading> createState() => _LoadingState();
+}
+
+class _LoadingState extends State<Loading> {
+
+  void setupWorldTIme() async {
+    WorldTime instance =WorldTime(location: 'Berlin',flag: 'germany.png',url: 'Europe/Berlin');
+    await instance.getTime();
+    Navigator.pushReplacementNamed(context, '/home',arguments: {
+      'location': instance.location,
+      'flag': instance.flag,
+      'time': instance.time,
+      'isDaytime': instance.isDaytime,
+    });
+  }
+  @override
+  void initState() {
+    super.initState();
+    setupWorldTIme();
+  }
+
+  Widget spinkit = SpinKitFadingCircle(
+    color: Colors.white,
+    size: 50.0,
+  );
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      body: Center(
+        child: spinkit,
+      )
+    );
+  }
+}
